ced740fb00981761227149946479c357ef031cf8
Trey Allen <trevon.allen1994@gmail.com>
2023-02-20 09:22:04 -0600

    Addition of Photo and about page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header';
 import Hero from '../components/Hero';
+import About from '../components/About';
 
 
 
@@ -20,6 +21,9 @@ const Home: NextPage = () => {
         </section>
 
         {/*About*/}
+        <section id="about" className="snap-center">
+            <About/>
+        </section>
 
         {/*Experience*/}
 
