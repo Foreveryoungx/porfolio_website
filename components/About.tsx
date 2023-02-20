6b6af441f1a64ec9b39982b60e71d9a57c7f6dbf
Trey Allen <trevon.allen1994@gmail.com>
2023-02-20 10:17:01 -0600

    Update About Me

--- components/About.tsx
+++ components/About.tsx
@@ -13,14 +13,32 @@ export default function About({}: Props) {
 
             <motion.img
                 initial={{
-                    x:-200
+                    x:-200,
+                    opacity:0
             }}
+                transition={{
+                    duration: 1.2,
+                }}
                 whileInView={{
                     x:0
                 }}
                 viewport={{once: true}}
                 src={"https://res.cloudinary.com/dvfdkfitt/image/upload/v1676901197/1660612160481_hg08ko.jpg"}
+                className = {"-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"}
                 />
+            <div className="space-y-10 px-0 md:px-10">
+                <h4 className={"text-4xl font-semibold"}>
+                    Here is a <span className={"underline decoration-[#F7AB0A]/50"}>little</span> background
+                </h4>
+                <p className={"text-base"}>
+                    Im Trey.😃  I am a driven and multi-talented individual with a deep passion for coding, bowling, gaming, and watching movies.
+                    I love to explore new coding languages, experiment with new coding projects, and get a group of friends together for a night of bowling and video gaming.
+                    In my free time, you can catch me watching movies and catching up on the latest TV series.
+                    I also take pride in being active in my community and enjoy giving back through volunteering and participating in local events.
+                    Despite my many interests, I am very family oriented and value my time spent with loved ones above all else.
+                    My determination, creativity, and caring nature make me a standout individual in any setting.
+                </p>
+            </div>
         </div>
     );
 };
