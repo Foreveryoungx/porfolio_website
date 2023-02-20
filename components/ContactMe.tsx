35f9a6960f7e1993afdd1742d0c34d396dad9c8c
Trey Allen <trevon.allen1994@gmail.com>
2023-02-20 12:53:32 -0600

    Contact page update

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -15,19 +15,36 @@ export default function ContactMe({}: Props) {
                     <span className={"decoration-[#f7AB0A]/50 underline"}>Lets talk.</span>
                 </h4>
 
-                <div>
+                <div className={"space-y-10"}>
                     <div className={"flex items-center space-x-5 justify-center"}>
                         <PhoneIcon className={"text-[#F7AB0A] h-7 w-7 animate-pulse"} />
                         <p className={"text-2xl"}>+16362147938</p>
                     </div>
                     <div className={"flex items-center space-x-5 justify-center"}>
                         <MapPinIcon className={"text-[#F7AB0A] h-7 w-7 animate-pulse"} />
-                        <p className={"text-2xl"}>123 Developer Lane</p>
+                        <p className={"text-2xl"}>314 Developer Lane</p>
                     </div>
                     <div className={"flex items-center space-x-5 justify-center"}>
                         <EnvelopeIcon className={"text-[#F7AB0A] h-7 w-7 animate-pulse"} />
                         <p className={"text-2xl"}>trevon.allen1994@gmail.com</p>
                     </div>
+
+                    <form className={"flex flex-col space-y-2 w-fit mx-auto"}>
+                        <div className={"flex space-x-2"}>
+                            <input placeholder="Name" className={"contactInput"} type={"text"}/>
+                            <input placeholder="Email" className={"contactInput"} type={"text"}/>
+                        </div>
+
+                        <input placeholder="Subject" className={"contactInput"} type={"text"}/>
+
+                        <textarea placeholder="Message" className={"contactInput"} />
+                        <button
+                            type={"submit"}
+                            className={"bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg"}
+                        >
+                            Submit
+                        </button>
+                    </form>
                 </div>
             </div>
         </div>
