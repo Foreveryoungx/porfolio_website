7e508a6d1e93c47dbd6480cfaa7c52968a2dcbdf
Trey Allen <trevon.allen1994@gmail.com>
2023-02-20 12:32:43 -0600

    Contact page update

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {EnvelopeIcon, MapPinIcon, PhoneIcon} from '@heroicons/react/20/solid';
 
 type Props = {}
 export default function ContactMe({}: Props) {
@@ -10,9 +11,24 @@ export default function ContactMe({}: Props) {
 
             <div className={"flex flex-col space-y-10"}>
                 <h4 className={"text-4xl font-semibold text-center"}>
-                    I have got justify what you need.{""}
+                    I have got just what you need.{""}
                     <span className={"decoration-[#f7AB0A]/50 underline"}>Lets talk.</span>
                 </h4>
+
+                <div>
+                    <div className={"flex items-center space-x-5 justify-center"}>
+                        <PhoneIcon className={"text-[#F7AB0A] h-7 w-7 animate-pulse"} />
+                        <p className={"text-2xl"}>+16362147938</p>
+                    </div>
+                    <div className={"flex items-center space-x-5 justify-center"}>
+                        <MapPinIcon className={"text-[#F7AB0A] h-7 w-7 animate-pulse"} />
+                        <p className={"text-2xl"}>123 Developer Lane</p>
+                    </div>
+                    <div className={"flex items-center space-x-5 justify-center"}>
+                        <EnvelopeIcon className={"text-[#F7AB0A] h-7 w-7 animate-pulse"} />
+                        <p className={"text-2xl"}>trevon.allen1994@gmail.com</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
